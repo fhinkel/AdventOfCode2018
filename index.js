3892f2ee3c626a3a1c231da4400310f5a4e0d959
Franziska Hinkelmann <franziska.hinkelmann@gmail.com>
2018-12-03 06:27:20 -0500

    Part 2, day 3

--- index.js
+++ index.js
@@ -9,56 +9,43 @@ let readInput = async () => {
     return inputs
 }
 
-let distance = (s1, s2) => {
-    let d = 0;
-    for (let i = 0; i < s1.length; i++) {
-        if (s1[i] !== s2[i]) {
-            d++;
-            if (d > 1) {
-                return d;
-            }
-        }
-    }
-
-    return d;
-}
-
-let findMultiples = (s) => {
-    let m = new Map();
-    for (let c of s.split('')) {
-        m.set(c, 1 + (m.get(c) || 0));
-    }
-    const counts = [...m.values()];
-    return [counts.includes(2), counts.includes(3)];
-}
-
 let overlap = async (inputs) => {
+    let validIds = new Set();
     let fabric = [];
-    for (let i = 0; i < 1000; i++) {
-        fabric[i] = [];
+    for (let w = 0; w < 1000; w++) {
+        fabric[w] = [];
+        for (let h = 0; h < 1000; h++) {
+            fabric[w][h] = [];
+        }
     }
     for (let input of inputs) {
         // #1 @ 1,3: 4x4
-        let [, , start, square] = input.split(' ');
+        let [id, , start, square] = input.split(' ');
+        id = Number(id.substring(1));
         let [left, top] = start.slice(0, -1).split(',').map(Number);
         let [width, height] = square.split('x').map(Number);
 
+        validIds.add(id);
         for (let w = 0; w < width; w++) {
             for (let h = 0; h < height; h++) {
-                fabric[w + left][h + top] = 1 + (fabric[w + left][h + top] || 0);
+                fabric[w + left][h + top].push(id);
+                let square = fabric[w + left][h + top];
+                if (square.length > 1) {
+                    for(let invalidId of square) {
+                        // console.log(`Delete ${invalidId}`);
+                        validIds.delete(invalidId);
+                    }
+                }
             }
         }
     }
-
-    let overlap = 0;
-    for (let w = 0; w < 1000; w++) {
-        for (let h = 0; h < 1000; h++) {
-            if (fabric[w][h] > 1) {
-                overlap++;
-            }
-        }
+    
+    // console.log(validIds.values());
+    if (validIds.size !==1) {
+        console.log('More than one valid Id');
+        return;
     }
-    return overlap;
+    return validIds.values().next().value;
 }
 
 let main = async () => {
