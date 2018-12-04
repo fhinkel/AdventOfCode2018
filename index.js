705267c3e7c712f4374755e9880a596af132e59d
Franziska Hinkelmann <franziska.hinkelmann@gmail.com>
2018-12-04 05:10:13 -0800

    Cleanup solution day 4

--- index.js
+++ index.js
@@ -5,59 +5,7 @@ let readInput = async () => {
     let res = await fs.readFile('./input4.txt');
     // let res = await fs.readFile('./testInput.txt');
     let inputs = res.toString().split('\n');
-    // console.log(inputs);
     return inputs;
-}
-
-let sleepsTheMost = (inputs) => {
-    // <guard id, total sleep minutes>
-    let sleeps = new Map();
-    let currentGuard;
-    let fallsAsleep;
-    inputs.forEach(s => {
-        // [1518-11-01 23:58] Guard #99 begins shift
-        let newGuard = s.match(/#(?<id>\d+)/);
-        if (newGuard) {
-            currentGuard = newGuard.groups.id;
-            // console.log(`New guard ${newGuard.groups.id}`);
-
-        } else {
-            // [1518-11-02 00:40] falls asleep
-            // [1518-11-02 00:50] wakes up
-            let m = s.match(/(?<hour>\d+):(?<minutes>\d+)]/);
-            if (m.groups.hour !== '00') {
-                console.log(`Sleep time is wrong: ${m.groups.hour}`);
-                console.log(s);
-                return;
-            }
-            let minutes = Number(m.groups.minutes);
-
-            if (s.match(/falls asleep/)) {
-                fallsAsleep = minutes;
-            } else if (s.match(/wakes up/)) {
-                let wakesUp = minutes;
-                let total = wakesUp - fallsAsleep;
-                if (total < 0) {
-                    console.log(`Negative sleep? ${minutes} - ${fallsAsleep}`);
-                    console.log(s);
-                    return;
-                }
-                sleeps.set(currentGuard, total + (sleeps.get(currentGuard) || 0));
-            }
-        }
-    })
-    // console.log([...sleeps.entries()]); // #1217 sleeps the most
-
-    let id;
-    let maxValue = -1;
-
-    [...sleeps.entries()].forEach(([k, v]) => {
-        if (v > maxValue) {
-            maxValue = v;
-            id = k;
-        }
-    })
-    return id;
 }
 
 let findMostSleepyMinute = (inputs) => {
@@ -69,13 +17,12 @@ let findMostSleepyMinute = (inputs) => {
     inputs.forEach(s => {
         // [1518-11-01 23:58] Guard #99 begins shift
         if (s.match(/#/)) {
-            let { groups: { id } } = s.match(/#(?<id>\d+)/);
-            currentGuard = id;
+            ({ groups: { id: currentGuard } } = s.match(/#(?<id>\d+)/));
         } else {
             // [1518-11-02 00:40] falls asleep
             // [1518-11-02 00:50] wakes up
-            let m = s.match(/(?<hour>\d+):(?<minutes>\d+)]/);
-            let minutes = Number(m.groups.minutes);
+            let {groups: {minutes}} = s.match(/\d+:(?<minutes>\d+)]/);
+            minutes = Number(minutes);
 
             if (s.match(/falls asleep/)) {
                 fallsAsleep = minutes;
