b961d2094bf7568bd90a53ddf02e555391a23eb4
Franziska Hinkelmann <franziska.hinkelmann@gmail.com>
2018-12-04 04:04:27 -0800

    Find guard that sleeps the most, day 4

--- index.js
+++ index.js
@@ -2,56 +2,67 @@ const fetch = require('node-fetch');
 const fs = require('fs').promises;
 
 let readInput = async () => {
-    let res = await fs.readFile('./input3.txt');
-    // let res = await fs.readFile('./testInput.txt');
+    // let res = await fs.readFile('./input4.txt');
+    let res = await fs.readFile('./testInput.txt');
     let inputs = res.toString().split('\n');
     // console.log(inputs);
-    return inputs
+    return inputs;
 }
 
-let overlap = async (inputs) => {
-    let validIds = new Set();
-    let fabric = [];
-    for (let w = 0; w < 1000; w++) {
-        fabric[w] = [];
-        for (let h = 0; h < 1000; h++) {
-            fabric[w][h] = [];
-        }
-    }
-    for (let input of inputs) {
-        // #1 @ 1,3: 4x4
-        let [id, , start, square] = input.split(' ');
-        id = Number(id.substring(1));
-        let [left, top] = start.slice(0, -1).split(',').map(Number);
-        let [width, height] = square.split('x').map(Number);
+let bestMinute = (inputs) => {
+    inputs.sort();
 
-        validIds.add(id);
-        for (let w = 0; w < width; w++) {
-            for (let h = 0; h < height; h++) {
-                fabric[w + left][h + top].push(id);
-                let square = fabric[w + left][h + top];
-                if (square.length > 1) {
-                    for(let invalidId of square) {
-                        // console.log(`Delete ${invalidId}`);
-                        validIds.delete(invalidId);
-                    }
+    // <guard id, total sleep minutes>
+    let sleeps = new Map();
+    let currentGuard;
+    let fallsAsleep;
+    inputs.forEach(s => {
+        // [1518-11-01 23:58] Guard #99 begins shift
+        let newGuard = s.match(/#(?<id>\d+)/);
+        if(newGuard) {
+            currentGuard = newGuard.groups.id;
+            // console.log(`New guard ${newGuard.groups.id}`);
+
+        } else {
+            // [1518-11-02 00:40] falls asleep
+            // [1518-11-02 00:50] wakes up
+            let m = s.match(/(?<hour>\d+):(?<minutes>\d+)]/);
+            if (m.groups.hour !== '00') {
+                console.log(`Sleep time is wrong: ${m.groups.hour}`);
+                console.log(s);
+                return;
+            }
+            let minutes = Number(m.groups.minutes);
+
+            if(s.match(/falls asleep/)) {
+                fallsAsleep = minutes;
+            } else if (s.match(/wakes up/)) {
+                let wakesUp = minutes;
+                let total = wakesUp - fallsAsleep;
+                if (total < 0) {
+                    console.log(`Negative sleep? ${minutes} - ${fallsAsleep}`);
+                    console.log(s);
+                    return;
                 }
+                console.log(total);
+                sleeps.set(currentGuard, total + (sleeps.get(currentGuard) || 0));
             }
         }
-    }
+    })
+    console.log([...sleeps.entries()]);
+        
     
-    // console.log(validIds.values());
-    if (validIds.size !==1) {
-        console.log('More than one valid Id');
-        return;
-    }
-    return validIds.values().next().value;
+
+    let minute = 0;
+    
+    return minute;
 }
+
 
 let main = async () => {
     let inputs = await readInput();
 
-    let res = await overlap(inputs);
+    let res = await bestMinute(inputs);
     console.log(res);
 }
 
