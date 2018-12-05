26929d91a04549deafc54aa08e51d284ef486885
Franziska Hinkelmann <franziska.hinkelmann@gmail.com>
2018-12-05 05:33:04 -0800

    Solution part 1, day 5

--- day5.js
+++ day5.js
@@ -2,34 +2,42 @@ const fetch = require('node-fetch');
 const fs = require('fs').promises;
 
 let readInput = async () => {
-    // let res = await fs.readFile('./input5.txt');
-    let res = await fs.readFile('./testInput.txt');
+    let res = await fs.readFile('./input5.txt');
+    // let res = await fs.readFile('./testInput.txt');
     let inputs = res.toString().split('\n');
     return inputs;
 }
 
 let minifyInput = (s) => {
-    let reacted = [];
-    let i = 0;
-    while (i < s.length - 1) {
-        let left = s[i];
-        let right = s[i + 1];
-        if ((left !== right) && (left.toUpperCase() === right.toUpperCase())) {
-            // console.log(`Delete ${s[i]} at ${i}`);
-            reacted.push(i);
-            i++;
+
+    let workLeft = true;
+
+    while (workLeft) {
+        let reacted = [];
+        let i = 0;
+        while (i < s.length - 1) {
+            let left = s[i];
+            let right = s[i + 1];
+            if ((left !== right) && (left.toUpperCase() === right.toUpperCase())) {
+                // console.log(`Delete ${s[i]} at ${i}`);
+                reacted.push(i);
+                i++;
+            }
+            i++
         }
-        i++
-    }
 
+        // console.log(`These need to be deleted: ${reacted}`);
+        for (let i = reacted.length - 1; i >= 0; i--) {
+            // delete i-1 and i
+            let leftString = s.slice(0, reacted[i]);
+            let rightString = s.slice(reacted[i] + 2)
+            // console.log(`${reacted[i]}: ${leftString} and ${rightString}`)
+            s = leftString + rightString;
+        }
+        if(reacted.length === 0) {
+            workLeft = false;
+        }
 
-    console.log(`These need to be deleted: ${reacted}`);
-    for (let i = reacted.length - 1; i >= 0; i--) {
-        // delete i-1 and i
-        let leftString = s.slice(0, reacted[i]);
-        let rightString = s.slice(reacted[i] + 2)
-        console.log(`${reacted[i]}: ${leftString} and ${rightString}`)
-        s = leftString + rightString;
     }
 
     return s;
@@ -43,6 +51,7 @@ let main = async () => {
     console.log(inputs[0]);
     let res = minifyInput(inputs[0]);
     console.log(res);
+    console.log(res.length);
 
 }
 
