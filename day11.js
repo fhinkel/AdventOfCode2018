750efa350e95d25f965654654dcbbffbc72c973c
Franziska Hinkelmann <franziska.hinkelmann@gmail.com>
2018-12-11 17:12:00 -0500

    Day 11, part 2

--- day11.js
+++ day11.js
@@ -5,65 +5,55 @@ let powerLevel = (x, y, serialNumber) => {
     return power - 5;
 }
 
-let main = async () => {
-    const GRIDSIZE = 300;
-    let serialNumber = 18 // 7672;
+const GRIDSIZE = 300;
+let serialNumber = 7672;
 
-    let b = [];
-    for (let i = 1; i <= GRIDSIZE; i++) {
-        b[i] = [];  // Add one row
-    }
-
-
-    for (let i = 1; i <= GRIDSIZE; i++) {
-        for (let j = 1; j <= GRIDSIZE; j++) {
-            b[i][j] = powerLevel(i, j, serialNumber);
-        }
-    }
-
-    let subSums = [];
-    for (let i = 1; i <= GRIDSIZE; i++) {
-        subSums[i] = [];  // Add one row
-        for (let j = 1; j <= GRIDSIZE; j++) {
-            subSums[i][j] = b[i][j];
-        }
-    }
-
-
-    let max = Number.MIN_SAFE_INTEGER;
-    let bestX;
-    let bestY;
-    let sum;
-    let bestSize = 1;
-
-
-    for (let size = 1; size <= GRIDSIZE; size++) {
-        for (let i = 1; i <= GRIDSIZE - size + 1; i++) {
-            for (let j = 1; j <= GRIDSIZE - size + 1; j++) {
-                let sum = subSums[i][j];
-                for (let h = 0; h < size; h++) {
-                    // if(!b[i+h]) {
-                    //     console.log(i,h,i+h)
-                    // }
-                    // if(!b[i+size]) {
-                    //     console.log(i, size, i+size)
-                    // }
-                    sum += b[i + h][j + size - 1] + b[i + size - 1][j + h];
-                }
-                sum -= b[i+size-1][j+size-1]; // delete right,lower corner
-                if (sum > max) {
-                    max = sum;
-                    bestX = i;
-                    bestY = j;
-                    bestSize = size;
-                }
-                subSums[i][j] = sum;
-            }
-        }
-    }
-
-    console.log(bestX, bestY, bestSize); // 90,269, 16 // 21,61
+let b = [];
+for (let i = 1; i <= GRIDSIZE; i++) {
+    b[i] = [];  // Add one row
 }
 
 
-main();
+for (let i = 1; i <= GRIDSIZE; i++) {
+    for (let j = 1; j <= GRIDSIZE; j++) {
+        b[i][j] = powerLevel(i, j, serialNumber);
+    }
+}
+
+// contains sum of square with upper, left corner i,j
+let subSums = [];
+for (let i = 1; i <= GRIDSIZE; i++) {
+    subSums[i] = [];  // Add one row
+    for (let j = 1; j <= GRIDSIZE; j++) {
+        subSums[i][j] = b[i][j];
+    }
+}
+
+
+let max = Number.MIN_SAFE_INTEGER;
+let bestX;
+let bestY;
+let sum;
+let bestSize = 1;
+
+
+for (let size = 1; size <= GRIDSIZE; size++) {
+    for (let i = 1; i <= GRIDSIZE - size + 1; i++) {
+        for (let j = 1; j <= GRIDSIZE - size + 1; j++) {
+            let sum = subSums[i][j];
+            for (let h = 0; h < size; h++) {
+                sum += b[i + h][j + size - 1] + b[i + size - 1][j + h];
+            }
+            sum -= b[i + size - 1][j + size - 1]; // delete right,lower corner
+            if (sum > max) {
+                max = sum;
+                bestX = i;
+                bestY = j;
+                bestSize = size;
+            }
+            subSums[i][j] = sum;
+        }
+    }
+}
+
+console.log(bestX, bestY, bestSize);
