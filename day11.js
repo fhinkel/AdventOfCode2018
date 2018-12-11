6a5e57e9f1e920cb9c01d196a1bec23131af3c33
Franziska Hinkelmann <franziska.hinkelmann@gmail.com>
2018-12-11 16:28:01 -0500

    Works on examples

--- day11.js
+++ day11.js
@@ -6,8 +6,8 @@ let powerLevel = (x, y, serialNumber) => {
 }
 
 let main = async () => {
-    const GRIDSIZE = 5; // 300
-    let serialNumber = 8; // 7672
+    const GRIDSIZE = 300;
+    let serialNumber = 42; // 7672
     // 3,5 serial number 8 => 4
     // 122,79, grid serial number 57: power level -5.
     // Fuel cell at 217,196, grid serial number 39: power level  0.
@@ -18,8 +18,36 @@ let main = async () => {
     console.log(`${217}, ${196} = ${powerLevel(217, 196, 39)} should be 0`)
     console.log(`${101}, ${153} = ${powerLevel(101, 153, 71)} should be 4`)
 
+    let b = [];
+    for (let i = 1; i <= GRIDSIZE; i++) {
+        b[i] = [];  // Add one row
+    }
 
-    console.log();
+
+    for (let i = 1; i <= GRIDSIZE; i++) {
+        for (let j = 1; j <= GRIDSIZE; j++) {
+            b[i][j] = powerLevel(i, j, serialNumber);
+        }
+    }
+
+    let max = Number.MIN_SAFE_INTEGER;
+    let bestX;
+    let bestY;
+    let sum;
+    for (let i = 1; i <= GRIDSIZE - 2; i++) {
+        for (let j = 1; j <= GRIDSIZE - 2; j++) {
+            sum = b[i][j] + b[i + 1][j] + b[i + 2][j] +
+                b[i][j + 1] + b[i + 1][j + 1] + b[i + 2][j + 1] +
+                b[i][j + 2] + b[i + 1][j + 2] + b[i + 2][j + 2];
+            if (sum > max) {
+                max = sum;
+                bestX = i;
+                bestY = j;
+            }
+        }
+    }
+
+    console.log(bestX, bestY); // 33,45 // 21,61
 }
 
 main();
