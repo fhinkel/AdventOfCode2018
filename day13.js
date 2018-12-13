b31ccdbeb48c6b2b83bce53f8b8dc0f4ef2ac380
Franziska Hinkelmann <franziska.hinkelmann@gmail.com>
2018-12-13 09:19:03 -0500

    Works on test input

--- day13.js
+++ day13.js
@@ -18,6 +18,7 @@ let move = (cart, grid) => {
 
     let [x, y] = [cart.x, cart.y];
     if (cart.d === 'u') {
+        // console.log(`up for ${cart.x}, ${cart.y}`);
         cart.y = cart.y - 1;
         let nextTrack = grid[y - 1][x];
         if (nextTrack === '/') {
@@ -38,7 +39,7 @@ let move = (cart, grid) => {
             throw new Error(`wrong direction, cart ${cart.x}, ${cart.y} going ${cart.d}`);
         }
     }
-    if (cart.d === 'd') {
+    else if (cart.d === 'd') {
         cart.y = cart.y + 1;
         let nextTrack = grid[y + 1][x];
         if (nextTrack === '/') {
@@ -59,7 +60,7 @@ let move = (cart, grid) => {
             throw new Error(`wrong direction, cart ${cart.x}, ${cart.y} going ${cart.d}`);
         }
     }
-    if (cart.d === 'l') {
+    else if (cart.d === 'l') {
         cart.x = cart.x - 1;
         let nextTrack = grid[y][x - 1];
         if (nextTrack === '/') {
@@ -80,13 +81,13 @@ let move = (cart, grid) => {
             throw new Error(`wrong direction, cart ${cart.x}, ${cart.y} going ${cart.d}`);
         }
     }
-    if (cart.d === 'r') {
+    else if (cart.d === 'r') {
         cart.x = cart.x + 1;
         let nextTrack = grid[y][x + 1];
         if (nextTrack === '/') {
             cart.d = 'u';
         } else if (nextTrack === '\\') {
-            cart.d = 'u';
+            cart.d = 'd';
         } else if (nextTrack === '+') {
             if (cart.n === 'cw') {
                 cart.d = 'd';
@@ -148,6 +149,22 @@ initializeGrid = (inputs) => {
     return [grid, carts];
 }
 
+let collision = (carts) => {
+    let x = -1;
+    let y = -1;
+
+    for (let i = carts.length - 1; i >= 0; i--) {
+        let currentCart = carts[i];
+        let index = carts.findIndex((cart) => {
+            return cart.x === currentCart.x && cart.y === currentCart.y;
+        });
+        if (index !==i ) {
+            return [currentCart.x, currentCart.y]
+        } 
+    }
+    return [x, y];
+}
+
 let main = async () => {
     let inputs = (await readInput());
     let [grid, carts] = initializeGrid(inputs);
@@ -160,13 +177,27 @@ let main = async () => {
             return c1.y - c2.y;
         }
         return c1.x - c2.x;
-    })
-
+    });
 
     console.log(orderedCarts);
-    for (let cart of orderedCarts) {
-        cart = move(cart, grid);
-        
+    let i = 15;
+    while (i > 0) {
+        i--;
+        for (let cart of orderedCarts) {
+            cart = move(cart, grid);
+            let [x, y] = collision(orderedCarts);
+            if (x > -1) {
+                console.log(`Crash! ${x}, ${y}`);
+                return;
+            }
+        }
+        orderedCarts.sort((c1, c2) => {
+            if (c1.y !== c2.y) {
+                return c1.y - c2.y;
+            }
+            return c1.x - c2.x;
+        });
+
     }
 }
 
