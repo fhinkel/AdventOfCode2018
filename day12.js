9ac057eac742fc9a9459f14aa8adad4c62dbfbe2
Franziska Hinkelmann <franziska.hinkelmann@gmail.com>
2018-12-12 19:44:09 -0500

    Correct sum part 1

--- day12.js
+++ day12.js
@@ -7,11 +7,64 @@ let readInput = async () => {
     return inputs
 }
 
+let nextGeneration = (initialState, rules) => {
+    initialState = '....' + initialState + '....';
+    next = '';
+
+    for (let i = 2; i < initialState.length - 2; i++) {
+        let substr = initialState.slice(i - 2, i + 3);
+        let pot = rules.get(substr) || '.';
+        // console.log(`${substr}: ${pot}`);
+        next += pot;
+    }
+    return next;
+}
+
 let main = async () => {
     let inputs = (await readInput());
+    //     initial state: #..#.#..##......###...###
+    //
+    // ...## => #
+    // ..#.. => #
+    let initialState;
+    // <pattern, next>
+    let rules = new Map();
     for (let line of inputs) {
-       
+        if (line.match(/initial state/)) {
+            initialState = line.split(' ')[2];
+            continue;
+        }
+        let [old, , next] = line.split(' ');
+        rules.set(old, next || '.');
     }
+
+    let next;
+    let left = 0;
+    let padding = '';
+    for (let i = 0; i < 2 * 20; i++) {
+        padding += '.';
+    }
+
+    console.log(padding + initialState + padding);
+    for (let i = 0; i < 20; i++) {
+        left -= 2;
+        next = nextGeneration(initialState, rules);
+        padding = padding.slice(2, padding.length - 2);
+        console.log(padding + next + padding);
+        initialState = next;
+    }
+
+    let sum = 0;
+    let index = left;
+    for (let i = 0; i < next.length; i++) {
+
+        if (next[i] === '#') {
+            sum += index;
+        }
+        index++;
+    }
+
+    console.log(sum)
 }
 
 main();
