4635d9c9cc8092a18bd448944dfd9faf9d9573b4
Franziska Hinkelmann <franziska.hinkelmann@gmail.com>
2018-12-12 20:38:54 -0500

    Clean up day 12

--- day12.js
+++ day12.js
@@ -8,20 +8,18 @@ let readInput = async () => {
 }
 
 let nextGeneration = (initialState, rules) => {
-    initialState = '..' + initialState + '....';
+    initialState = '..' + initialState + '...';
     next = '';
 
     for (let i = 2; i < initialState.length - 2; i++) {
         let substr = initialState.slice(i - 2, i + 3);
         let pot = rules.get(substr) || '.';
-        // console.log(`${substr}: ${pot}`);
         next += pot;
     }
     return next;
 }
 
 let main = async () => {
-    ``
     let inputs = (await readInput());
     //     initial state: #..#.#..##......###...###
     //
@@ -39,32 +37,21 @@ let main = async () => {
         rules.set(old, next || '.');
     }
 
-    let left = 0;
-    let padding = '';
-    for (let i = 0; i < 2 * 20; i++) {
-        padding += '.';
-    }
-
     initialState = '.' + initialState;
     let next = initialState;
 
-    let prevPotArea = initialState.slice(initialState.indexOf('#'), initialState.lastIndexOf('#'));
+    let prevPlanArea = initialState.slice(initialState.indexOf('#'), initialState.lastIndexOf('#'));
     console.log(initialState);
     let i = 1;
     for (; i < 300; i++) {
-        left -= 2;
         next = nextGeneration(next, rules);
-        padding = padding.slice(0, padding.length - 2);
-        let potArea = next.slice(next.indexOf('#'), next.lastIndexOf('#'));
-        if (prevPotArea === potArea) {
+        let plantArea = next.slice(next.indexOf('#'), next.lastIndexOf('#'));
+        if (prevPlanArea === plantArea) {
             console.log(`We got stability ${i}`);
             break;
         }
-        prevPotArea = potArea;
-        // console.log( next + padding);
+        prevPlanArea = plantArea;
     }
-
-
 
     let sum = 0;
     let numberOfPlants = 0;
@@ -77,10 +64,7 @@ let main = async () => {
         index++;
     }
 
-    // let LARGE = 120;
-    let LARGE = 50000000000;
-    remaningIterations = LARGE - i;
-
+    remaningIterations = 50000000000 - i;
     console.log(numberOfPlants * remaningIterations + sum);
 }
 
