3fae722e0ee532820281a37afc9e97c85a12ce81
Franziska Hinkelmann <franziska.hinkelmann@gmail.com>
2018-12-13 08:39:21 -0500

    It compiles

--- day13.js
+++ day13.js
@@ -13,14 +13,103 @@ print = (grid) => {
     }
 }
 
-let main = async () => {
-    let inputs = (await readInput());
+let move = (cart, grid) => {
+    let nextTurns = ['ccw', 's', 'cw'];
+
+    let [x, y] = [cart.x, cart.y];
+    if (cart.d === 'u') {
+        cart.y = cart.y - 1;
+        let nextTrack = grid[y - 1][x];
+        if (nextTrack === '/') {
+            cart.d = 'r';
+        } else if (nextTrack === '\\') {
+            cart.d = 'l';
+        } else if (nextTrack === '+') {
+            if (cart.n === 'cw') {
+                cart.d = 'r';
+                cart.n = nextTurns[(nextTurns.indexOf(cart.n) + 1) % 3];
+            } else if (cart.n === 'ccw') {
+                cart.d = 'l';
+                cart.n = nextTurns[(nextTurns.indexOf(cart.n) + 1) % 3];
+            } else if (cart.n === 's') {
+                cart.n = nextTurns[(nextTurns.indexOf(cart.n) + 1) % 3];
+            }
+        } else if (nextTrack === '-') {
+            throw new Error(`wrong direction, cart ${cart.x}, ${cart.y} going ${cart.d}`);
+        }
+    }
+    if (cart.d === 'd') {
+        cart.y = cart.y + 1;
+        let nextTrack = grid[y + 1][x];
+        if (nextTrack === '/') {
+            cart.d = 'l';
+        } else if (nextTrack === '\\') {
+            cart.d = 'r';
+        } else if (nextTrack === '+') {
+            if (cart.n === 'cw') {
+                cart.d = 'l';
+                cart.n = nextTurns[(nextTurns.indexOf(cart.n) + 1) % 3];
+            } else if (cart.n === 'ccw') {
+                cart.d = 'r';
+                cart.n = nextTurns[(nextTurns.indexOf(cart.n) + 1) % 3];
+            } else if (cart.n === 's') {
+                cart.n = nextTurns[(nextTurns.indexOf(cart.n) + 1) % 3];
+            }
+        } else if (nextTrack === '-') {
+            throw new Error(`wrong direction, cart ${cart.x}, ${cart.y} going ${cart.d}`);
+        }
+    }
+    if (cart.d === 'l') {
+        cart.x = cart.x - 1;
+        let nextTrack = grid[y][x - 1];
+        if (nextTrack === '/') {
+            cart.d = 'd';
+        } else if (nextTrack === '\\') {
+            cart.d = 'u';
+        } else if (nextTrack === '+') {
+            if (cart.n === 'cw') {
+                cart.d = 'u';
+                cart.n = nextTurns[(nextTurns.indexOf(cart.n) + 1) % 3];
+            } else if (cart.n === 'ccw') {
+                cart.d = 'd';
+                cart.n = nextTurns[(nextTurns.indexOf(cart.n) + 1) % 3];
+            } else if (cart.n === 's') {
+                cart.n = nextTurns[(nextTurns.indexOf(cart.n) + 1) % 3];
+            }
+        } else if (nextTrack === '|') {
+            throw new Error(`wrong direction, cart ${cart.x}, ${cart.y} going ${cart.d}`);
+        }
+    }
+    if (cart.d === 'r') {
+        cart.x = cart.x + 1;
+        let nextTrack = grid[y][x + 1];
+        if (nextTrack === '/') {
+            cart.d = 'u';
+        } else if (nextTrack === '\\') {
+            cart.d = 'u';
+        } else if (nextTrack === '+') {
+            if (cart.n === 'cw') {
+                cart.d = 'd';
+                cart.n = nextTurns[(nextTurns.indexOf(cart.n) + 1) % 3];
+            } else if (cart.n === 'ccw') {
+                cart.d = 'u';
+                cart.n = nextTurns[(nextTurns.indexOf(cart.n) + 1) % 3];
+            } else if (cart.n === 's') {
+                cart.n = nextTurns[(nextTurns.indexOf(cart.n) + 1) % 3];
+            }
+        } else if (nextTrack === '|') {
+            throw new Error(`wrong direction, cart ${cart.x}, ${cart.y} going ${cart.d}`);
+        }
+    }
+    return cart;
+}
+
+initializeGrid = (inputs) => {
     let h = inputs.length;
     let w = inputs[0].length;
     console.log(`Working ${w} x ${h} grid`);
 
     let grid = [];
-
     let carts = [];
     for (let i = 0; i < h; i++) {
         grid[i] = [];
@@ -56,12 +145,30 @@ let main = async () => {
         }
     }
 
+    return [grid, carts];
+}
+
+let main = async () => {
+    let inputs = (await readInput());
+    let [grid, carts] = initializeGrid(inputs);
+
     print(grid);
     console.log(carts);
 
+    let orderedCarts = carts.slice().sort((c1, c2) => {
+        if (c1.y !== c2.y) {
+            return c1.y - c2.y;
+        }
+        return c1.x - c2.x;
+    })
 
 
-    console.log();
+    console.log(orderedCarts);
+    for (let cart of orderedCarts) {
+        cart = move(cart, grid);
+        
+    }
 }
+
 
 main();
