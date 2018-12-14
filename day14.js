5ec0dce568f468053bc07575de6726257ee4ce74
Franziska Hinkelmann <franziska.hinkelmann@gmail.com>
2018-12-14 08:27:21 -0500

    Works on first example

---
+++ day14.js
@@ -0,0 +1,31 @@
+
+let main = () => {
+    const NUMBER_OF_RECIPES = 9;  // 765071;
+    let recipes = [3, 7];
+
+    let elf1 = 0;
+    let elf2 = 1;
+
+    while (recipes.length < NUMBER_OF_RECIPES + 10) {
+        let recipe1 = recipes[elf1];
+        let recipe2 = recipes[elf2];
+        let sum = recipe1 + recipe2;
+        if (sum > 9) {
+            recipes.push(Math.floor(sum / 10));
+            recipes.push(sum % 10);
+        } else {
+            recipes.push(sum);
+        }
+
+        // move elves
+        elf1 = (elf1 + 1 + recipe1) % recipes.length;
+        elf2 = (elf2 + 1 + recipe2) % recipes.length;
+    }
+
+    let score = recipes.slice(NUMBER_OF_RECIPES, NUMBER_OF_RECIPES + 10);
+    console.log(score.join(''));
+    // console.log(recipes);
+}
+
+
+main();
