bc034cdd2c2f29c3bd5e9fd778df206db652e3f5
Franziska Hinkelmann <franziska.hinkelmann@gmail.com>
2018-12-14 08:39:07 -0500

    Day 14, part 2

--- day14.js
+++ day14.js
@@ -1,31 +1,49 @@
 
+let matches = (recipes, pattern) => {
+    if (recipes.length < pattern.length) {
+        return false;
+    }
+
+    let tail = recipes.slice(- pattern.length);
+    return tail.join('') === pattern;
+}
+
 let main = () => {
-    const NUMBER_OF_RECIPES = 765071;
+    const PATTERN = '765071';
 
     let recipes = [3, 7];
-    
+
     let elf1 = 0;
     let elf2 = 1;
-    
-    while (recipes.length < NUMBER_OF_RECIPES + 10) {
+
+    while (true) {
         let recipe1 = recipes[elf1];
         let recipe2 = recipes[elf2];
         let sum = recipe1 + recipe2;
         if (sum > 9) {
             recipes.push(Math.floor(sum / 10));
+            if (matches(recipes, PATTERN)) {
+                break;
+            }
             recipes.push(sum % 10);
+            if (matches(recipes, PATTERN)) {
+                break;
+            }
         } else {
             recipes.push(sum);
+            if (matches(recipes, PATTERN)) {
+                break;
+            }
         }
-        
-        // move elves
+
+        // Move elves
         elf1 = (elf1 + 1 + recipe1) % recipes.length;
         elf2 = (elf2 + 1 + recipe2) % recipes.length;
     }
-    
-    let score = recipes.slice(NUMBER_OF_RECIPES, NUMBER_OF_RECIPES + 10);
-    console.log(score.join(''));
-    //51589 after 9
+
+    console.log(recipes.length - PATTERN.length);
+    // 51589 after 9
+    // 59414 after 2018
 }
 
 
