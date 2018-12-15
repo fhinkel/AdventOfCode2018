39de638c0dcf82b97d29f464306d270afc53a87e
Franziska Hinkelmann <franziska.hinkelmann@gmail.com>
2018-12-15 09:26:51 -0500

    Initialize starting point

--- day15.js
+++ day15.js
@@ -9,7 +9,30 @@ let readInput = async () => {
 
 let main = async () => {
     let inputs = (await readInput());
-    console.log();
+
+    let board = Array(inputs.length).fill().map(()=> []);
+    let elfs = new Set();
+    let goblins = new Set();
+    // {i, j, hitPoints = 200 }
+
+    for(let i = 0; i < inputs.length; i++) {
+        for(let j = 0; j < inputs[0].length; j++) {
+            board[i][j] = inputs[i][j];
+            if(inputs[i][j] === 'E') {
+                let elf = {i, j, hitPoints : 200 };
+                elfs.add(elf);
+            }
+            if(inputs[i][j] === 'G') {
+                let goblin = {i, j, hitPoints : 200 };
+                goblins.add(goblin);
+            }
+        }
+    }
+
+
+    console.log(board);
+    console.log(elfs);
+    console.log(goblins);
 }
 
 
