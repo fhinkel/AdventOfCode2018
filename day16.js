64ff0e14b64f19ec347c20bc3654c087e7c27fe5
Franziska Hinkelmann <franziska.hinkelmann@gmail.com>
2018-12-16 10:10:34 -0500

    Day 16, part 1

--- day16.js
+++ day16.js
@@ -2,16 +2,12 @@ const fs = require('fs').promises;
 
 let readInput = async () => {
     let res = await fs.readFile('./input16.txt');
+    // let res = await fs.readFile('./testInput.txt');
     let inputs = res.toString().split('\n');
     return inputs;
 }
 
-let main = async () => {
-    let inputs = (await readInput());
-    //Before: [0, 0, 2, 2]
-    // 9 2 3 0
-    // After:  [4, 0, 2, 2]
-
+let initialize = (inputs) => {
     let examples = [];
     let example = [];
 
@@ -33,32 +29,128 @@ let main = async () => {
             instruction = line.match(/\d+/g).map(Number);
             example.push(instruction);
         }
-
-        if (examples.length === 30) {
-            console.log(examples)
-            break;
-        }
-
     }
+    return examples;
+}
 
-    // add
-    // mult
-    // &
-    // |
-    // ass
-    // gtr
-    // equ
+let main = async () => {
+    let inputs = (await readInput());
+    let examples = initialize(inputs);
+
     let opcodes = [];
 
+    let opcodeAddr = (a, b, c, before) => {
+        // addi : reg + reg
+        before[c] = before[a] + before[b];
+        return before;
+    }
     let opcodeAddi = (a, b, c, before) => {
         // addi : reg + value
         before[c] = before[a] + b;
         return before;
     }
 
+    let opcodeMulr = (a, b, c, before) => {
+        // mul : reg * reg
+        before[c] = before[a] * before[b];
+        return before;
+    }
+    let opcodeMuli = (a, b, c, before) => {
+        // mul : reg * value
+        before[c] = before[a] * b;
+        return before;
+    }
+
+    let opcodeBanr = (a, b, c, before) => {
+        // bitwise And : reg & reg
+        before[c] = before[a] & before[b];
+        return before;
+    }
+    let opcodeBani = (a, b, c, before) => {
+        // bitwise And : reg & value
+        before[c] = before[a] & b;
+        return before;
+    }
+
+    let opcodeBorr = (a, b, c, before) => {
+        // bitwise Or : reg | reg
+        before[c] = before[a] | before[b];
+        return before;
+    }
+    let opcodeBori = (a, b, c, before) => {
+        // bitwise Or : reg | value
+        before[c] = before[a] | b;
+        return before;
+    }
+
+    let opcodeSetr = (a, b, c, before) => {
+        // assign: copies register a into c
+        before[c] = before[a];
+        return before;
+    }
+    let opcodeSeti = (a, b, c, before) => {
+        // assign: copies value a into c
+        before[c] = a;
+        return before;
+    }
+
+    let opcodeGtir = (a, b, c, before) => {
+        // value A greater than register B
+        before[c] = a > before[b] ? 1 : 0;
+        return before;
+    }
+    let opcodeGtri = (a, b, c, before) => {
+        // register A greater than value B
+        before[c] = before[a] > b ? 1 : 0;
+        return before;
+    }
+    let opcodeGtrr = (a, b, c, before) => {
+        // register A greater than register B
+        before[c] = before[a] > before[b] ? 1 : 0;
+        return before;
+    }
+
+    let opcodeEqir = (a, b, c, before) => {
+        // value A equal to register B
+        before[c] = a === before[b] ? 1 : 0;
+        return before;
+    }
+    let opcodeEqri = (a, b, c, before) => {
+        // register A equal to value B
+        before[c] = before[a] === b ? 1 : 0;
+        return before;
+    }
+    let opcodeEqrr = (a, b, c, before) => {
+        // register A equal to register B
+        before[c] = before[a] === before[b] ? 1 : 0;
+        return before;
+    }
+
+
+
+    opcodes.push(opcodeAddr);
     opcodes.push(opcodeAddi);
+    opcodes.push(opcodeMulr);
+    opcodes.push(opcodeMuli);
+    opcodes.push(opcodeBanr);
+    opcodes.push(opcodeBani);
+    opcodes.push(opcodeBorr);
+    opcodes.push(opcodeBori);
+    opcodes.push(opcodeSetr);
+    opcodes.push(opcodeSeti);
+    opcodes.push(opcodeGtir);
+    opcodes.push(opcodeGtri);
+    opcodes.push(opcodeGtrr);
+    opcodes.push(opcodeEqir);
+    opcodes.push(opcodeEqri);
+    opcodes.push(opcodeEqrr);
 
     let count = 0;
+
+    if (opcodes.length !== 16) {
+        console.log('Counting is hard');
+        return;
+    }
 
     let equalRegisters = (r1, r2) => {
         if (r1.length !== r2.length) {
@@ -74,14 +166,17 @@ let main = async () => {
 
     for (let [before, instruction, after] of examples) {
         let validOpCodes = [];
-        for (let opcode of opcodes) {
-            let res = opcode(instruction[1], instruction[2], instruction[3], before);
+        for (let i = 0; i < opcodes.length; i++) {
+            const opcode = opcodes[i];
+            let res = opcode(instruction[1], instruction[2], instruction[3], before.slice());
 
             if (equalRegisters(res, after)) {
+                // console.log('equal')
+                // console.log(i);
                 validOpCodes.push(opcode);
             }
         }
-        if (validOpCodes.length > 0) {
+        if (validOpCodes.length >= 3) {
             count++;
         }
     }
