96d6115ec5a92eb6e153e455228d28ea56c00a7d
Franziska Hinkelmann <franziska.hinkelmann@gmail.com>
2018-12-20 23:31:32 +0100

    Day 19, part 1

--- day19.js
+++ day19.js
@@ -1,146 +1,128 @@
 const fs = require('fs').promises;
 
-let SIZE = 50;
-
-
 let readInput = async () => {
-    let res = await fs.readFile('./input18.txt');
+    let res = await fs.readFile('./input19.txt');
     // let res = await fs.readFile('./testInput.txt');
     let inputs = res.toString().split('\n');
     return inputs;
 }
 
-
-let adjacent = (board, x, y) => {
-    let fields = [];
-    if (y > 0) {
-        if (x > 0) {
-            fields.push([y - 1, x - 1]);
-        }
-        fields.push([y - 1, x])
-        if (x < SIZE - 1) {
-            fields.push([y - 1, x + 1]);
-        }
-    }
-    if (x > 0) {
-        fields.push([y, x - 1]);
-    }
-    if (x < SIZE - 1) {
-        fields.push([y, x + 1]);
-    }
-    if (y < SIZE - 1) {
-        if (x > 0) {
-            fields.push([y + 1, x - 1]);
-        }
-        fields.push([y + 1, x])
-        if (x < SIZE - 1) {
-            fields.push([y + 1, x + 1]);
-        }
-    }
-
-    // [open spaces, trees, lumberyards]
-    let res = [0, 0, 0];
-    for (let [j, i] of fields) {
-        if (board[j][i] === '.') {
-            res[0] = res[0] + 1;
-        } else if (board[j][i] === '|') {
-            res[1] = res[1] + 1;
-        } else if (board[j][i] === '#') {
-            res[2] = res[2] + 1;
-        }
-    }
-
-    return res;
-}
-
-let newField = (board, x, y) => {
-    let [, tree, lumberyard] = adjacent(board, x, y);
-    if (board[y][x] === '.') {
-        if (tree >= 3) {
-            return '|';
-        } else {
-            return '.';
-        }
-    }
-
-    if (board[y][x] === '|') {
-        if (lumberyard >= 3) {
-            return '#';
-        } else {
-            return '|';
-        }
-    }
-    if (board[y][x] === '#') {
-        if (lumberyard === 0 || tree === 0) {
-            return '.';
-        } else {
-            return '#';
-        }
-    }
-}
-
-let getResourceNumber = (board) => {
-    let wood = 0;
-    let lumberyard = 0;
-    for (let i = 0; i < board.length; i++) {
-        for (let j = 0; j < board[0].length; j++) {
-            if (board[i][j] === '|') {
-                wood++;
-            } else if (board[i][j] === '#') {
-                lumberyard++;
-            }
-        }
-    }
-    return lumberyard * wood;
-}
-
 let main = async () => {
-
     let inputs = await readInput();
 
-    let board = Array(SIZE).fill().map(() => []);
-    let i = 0;
+    let opcodeAddr = (a, b, before) => {
+        // addi : reg + reg
+        return before[a] + before[b];
+    }
+    let opcodeAddi = (a, b, before) => {
+        // addi : reg + value
+        return before[a] + b;
+    }
+
+    let opcodeMulr = (a, b, before) => {
+        // mul : reg * reg
+        return before[a] * before[b];
+    }
+    let opcodeMuli = (a, b, before) => {
+        // mul : reg * value
+        return before[a] * b;
+    }
+
+    let opcodeBanr = (a, b, before) => {
+        // bitwise And : reg & reg
+        return before[a] & before[b];
+    }
+    let opcodeBani = (a, b, before) => {
+        // bitwise And : reg & value
+        return before[a] & b;
+    }
+
+    let opcodeBorr = (a, b, before) => {
+        // bitwise Or : reg | reg
+        return before[a] | before[b];
+    }
+    let opcodeBori = (a, b, before) => {
+        // bitwise Or : reg | value
+        return before[a] | b;
+    }
+
+    let opcodeSetr = (a, b, before) => {
+        // assign: copies register a into c
+        return before[a];
+    }
+    let opcodeSeti = (a, b, before) => {
+        // assign: copies value a into c
+        return a;
+    }
+
+    let opcodeGtir = (a, b, before) => {
+        // value A greater than register B
+        return a > before[b] ? 1 : 0;
+    }
+    let opcodeGtri = (a, b, before) => {
+        // register A greater than value B
+        return before[a] > b ? 1 : 0;
+    }
+    let opcodeGtrr = (a, b, before) => {
+        // register A greater than register B
+        return before[a] > before[b] ? 1 : 0;
+    }
+
+    let opcodeEqir = (a, b, before) => {
+        // value A equal to register B
+        return a === before[b] ? 1 : 0;
+    }
+    let opcodeEqri = (a, b, before) => {
+        // register A equal to value B
+        return before[a] === b ? 1 : 0;
+    }
+    let opcodeEqrr = (a, b, before) => {
+        // register A equal to register B
+        return before[a] === before[b] ? 1 : 0;
+    }
+
+    let opcodes = new Map();
+    opcodes.set('addi', opcodeAddi);
+    opcodes.set('addr', opcodeAddr);
+    opcodes.set('mulr', opcodeMulr);
+    opcodes.set('muli', opcodeMuli);
+    opcodes.set('banr', opcodeBanr);
+    opcodes.set('bani', opcodeBani);
+    opcodes.set('borr', opcodeBorr);
+    opcodes.set('bori', opcodeBori);
+    opcodes.set('setr', opcodeSetr);
+    opcodes.set('seti', opcodeSeti);
+    opcodes.set('gtir', opcodeGtir);
+    opcodes.set('gtri', opcodeGtri);
+    opcodes.set('gtrr', opcodeGtrr);
+    opcodes.set('eqir', opcodeEqir);
+    opcodes.set('eqri', opcodeEqri);
+    opcodes.set('eqrr', opcodeEqrr);
+
+    if (opcodes.size !== 16) {
+        console.log('Counting is hard');
+        return;
+    }
+    // #ip 0
+    let instructionRegister = Number(inputs.shift().split(' ')[1]);
+
+    let instructions = [];
     for (let line of inputs) {
-        board[i] = line.split('');
-        i++;
+        let [ops, a, b, output] = line.split(' ');
+        instructions.push([ops, Number(a), Number(b), Number(output)]);
     }
 
-    let resourceNumber = [];
-    let s = new Map();
-    let newBoard = Array(SIZE).fill().map(() => []);
-
-    let n = getResourceNumber(board);
-    resourceNumber.push(n);
-
-    let end = 2000;
-    for (let time = 0; time < end; time++) {
-        for (let i = 0; i < board.length; i++) {
-            for (let j = 0; j < board[0].length; j++) {
-                newBoard[i][j] = newField(board, j, i);
-            }
-        }
-        for (let i = 0; i < board.length; i++) {
-            board[i] = newBoard[i].slice();
-        }
-        let n = getResourceNumber(board);
-        resourceNumber.push(n);
+    let register = [0, 0, 0, 0, 0, 0];
+    let ip = 0;
+    while (ip < instructions.length && ip >= 0) {
+        let [ops, a, b, output] = instructions[ip];
+        register[output] = opcodes.get(ops)(a, b, register);
+        ip = register[instructionRegister];
+        ip = ip + 1;
+        register[instructionRegister] = ip;
     }
 
-    let last = resourceNumber[resourceNumber.length - 1];
-    let repeatIndex = resourceNumber.lastIndexOf(last, resourceNumber.length - 2)
-    let cycleLength = end - repeatIndex;
-    for (let i = end - cycleLength - cycleLength; i < end - cycleLength; i++) {
-        if (resourceNumber[i] !== resourceNumber[i + cycleLength]) {
-            console.log("no match");
-            throw new Error();
-        }
-    }
-
-    let B = 1000000000;
-
-    let index = (B - end) % cycleLength;
-
-    console.log(resourceNumber[repeatIndex - cycleLength + index]);
+    console.log(register[0]);
 }
 
 main();
