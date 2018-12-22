5746422e0ac1add3fc72e4b606a117fd72196cbd
Franziska Hinkelmann <franziska.hinkelmann@gmail.com>
2018-12-22 21:51:15 +0100

    Initial import day 22

---
+++ day22.js
@@ -0,0 +1,15 @@
+const fs = require('fs').promises;
+
+let readInput = async () => {
+    // let res = await fs.readFile('./input22.txt');
+    let res = await fs.readFile('./testInput.txt');
+    let inputs = res.toString().split('\n');
+    return inputs;
+}
+
+let main = async () => {
+    let inputs = await readInput();
+
+}
+
+main();
