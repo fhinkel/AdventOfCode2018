ef89b2d02ba6502f1c141ca36c1e3d3c7afd4fe1
Franziska Hinkelmann <franziska.hinkelmann@gmail.com>
2018-12-23 23:33:42 +0100

    Small refactorings

--- day23.js
+++ day23.js
@@ -10,6 +10,13 @@ let readInput = async () => {
 const SPACER = 100000000n;
 
 h = (x, y, z) => BigInt(x) * SPACER * SPACER + BigInt(y) * SPACER + BigInt(z);
+let unhash = (hash) => {
+    const z = hash % SPACER;
+    hash = (hash / SPACER);
+    const y = hash % SPACER
+    const x = (hash / SPACER);
+    return [x, y, z].map(Number);
+}
 let dist = (a, b) => {
     let res = 0;
     for (let i = 0; i < a.length; i++) {
@@ -18,7 +25,7 @@ let dist = (a, b) => {
     return res;
 }
 
-markAllPoints = (c, r, nanobots) => {
+markAllPoints = (c, r, numberOfBotsInRange) => {
     let points = new Set();
     for (let dx = 0; dx <= r; dx++) {
         for (let dy = 0; dy <= r - dx; dy++) { // sum = dy+dz
@@ -32,8 +39,8 @@ markAllPoints = (c, r, nanobots) => {
                 c[1] + dy * d[1],
                 c[2] + dz * d[2]];
                 if (!points.has(h(...point))) {
-                    let count = nanobots.get(h(...point)) || 0;
-                    nanobots.set(h(...point), count + 1);
+                    let count = numberOfBotsInRange.get(h(...point)) || 0;
+                    numberOfBotsInRange.set(h(...point), count + 1);
                 }
                 points.add(h(...point));
             }
@@ -43,22 +50,26 @@ markAllPoints = (c, r, nanobots) => {
 
 let main = async () => {
     let inputs = await readInput();
-
-    // < hash, number of bots in range>
     let nanobots = new Map();
+
     for (const line of inputs) {
         const m = line.match(/^pos=<(-?\d+),(-?\d+),(-?\d+)>, r=(\d+)$/)
         const [x, y, z] = [m[1], m[2], m[3]].map(Number);
         const r = Number(m[4]);
+        nanobots.set(h(x, y, z), r);
+    }
+
+    // < hash, number of bots in range>
+    let numberOfBotsInRange = new Map();
+    for (const [hash, r] of [...nanobots]) {
         for (let i = 0; i <= r; i++) {
-            markAllPoints([x, y, z], i, nanobots);
+            markAllPoints(unhash(hash), i, numberOfBotsInRange);
         }
-        // console.log(nanobots)
     }
 
     let maxCount = 0;
     let maxHashes = [];
-    for (const [hash, count] of [...nanobots.entries()]) {
+    for (const [hash, count] of [...numberOfBotsInRange.entries()]) {
         if (count > maxCount) {
             maxHashes = [];
             maxCount = count;
@@ -72,18 +83,19 @@ let main = async () => {
     let minDist = Number.POSITIVE_INFINITY;
     console.log(maxHashes);
 
+
+
     for (let hash of maxHashes) {
-        const z = hash % SPACER;
-        hash = (hash / SPACER);
-        const y = hash % SPACER
-        const x = (hash / SPACER);
+        const [x, y, z] = unhash(hash);
         let dist = x + y + z;
         if (dist < minDist) {
             points.push([x, y, z]);
         }
     }
     points.sort((a, b) => (a[0] + a[1] + a[2]) - (b[0] + b[1] + b[2]));
-    console.log(points[0]);
+    let destination = points[0];
+    console.log(destination.reduce((v,acc) => v + acc));
+
 
 }
 
