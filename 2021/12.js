582d9a1d8161157fc6a0f1f47d782d102f366b7a
Franziska Hinkelmann <franziska.hinkelmann@gmail.com>
2022-05-04 09:56:24 -0400

    work on part 2

--- 2021/12.js
+++ 2021/12.js
@@ -4,6 +4,7 @@ const readline = require('readline');
 const glob = require('glob');
 const { statSync } = require('fs');
 
+const isLower = (a) => a === a.toLocaleLowerCase();
 
 async function processLineByLine(file) {
     const fileStream = fs.createReadStream(file);
@@ -12,33 +13,68 @@ async function processLineByLine(file) {
         crlfDelay: Infinity,
     });
     // Note: we use the crlfDelay option to recognize all instances of CR LF
-    // ('\r\n') in input.txt as a single line break.
-
+    // ('\r\n') in input.txt as a single line break
 
     let connections = [];
     for await (const line of rl) {
         const [a, b] = line.split('-');
         connections.push([a, b], [b, a]);
+        if (isLower(a) && a !== 'start') {
+            const aCopy = a + `2`;
+            connections.push([aCopy, b], [b, aCopy]);
+        }
+        if (isLower(b) && b !== 'end') {
+            const bCopy = b + `2`;
+            connections.push([a, bCopy], [bCopy, a]);
+        }
+
     }
+
+    // console.log(connections);
 
     console.log(runPath('start', connections, ''));
 }
 
+const isSame = (a, b) => {
+    if (a === b) return true;
+    if ((a + '2') === b) return true;
+    if (a === (b + '2')) return true;
+    return false;
+};
 
-const runPath = (src, connections, path) => {
+const getOriginal = (a) => {
+    if (a.at(-1) === '2') {
+        a = a.slice(0, -1);
+    }
+    return a;
+}
+
+const isOriginal = (a) => {
+    return a === getOriginal(a);
+}
+
+const isCopy = a => !isOriginal(a);
+
+
+const runPath = (start, connections, path) => {
     let count = 0;
-    path = `${path} - ${src}`;
-    if (src === 'end') {
+    path = `${path} - ${start}`;
+    if (start === 'end') {
+        console.log(path);
         return 1;
     }
     for (const [a, b] of connections) {
-        if (a !== src) continue;
+        if (a !== start) continue;
+    
         const fewerConnections = [];
         for (const [src, dest] of connections) {
-            if (a.toLowerCase() === a && (src === a || dest === a)) continue;
+            // console.log(a, getOriginal(a) ,isOriginal(a), isCopy(a));
+            if (isCopy(a) && (isCopy(src) || isCopy(dest))) continue; // throw out all other copies if we used a copy
+            if (isLower(a) && (src === a || dest === a)) continue; // throw out all connections to already visited lowercase
             fewerConnections.push([src, dest]);
         }
-        count += runPath(b, fewerConnections, path);
+        // console.log(fewerConnections);
+        count += runPath(getOriginal(b), fewerConnections, path);
     }
     return count
 }
