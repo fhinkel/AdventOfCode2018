b37005b616200539f11cb0fbec5ca290f57edfda
Franziska Hinkelmann <franziska.hinkelmann@gmail.com>
2024-08-08 21:32:57 -0400

    day 1, part 2, add support for words representing numbers

--- 2023/1.js
+++ 2023/1.js
@@ -14,23 +14,53 @@ async function processLineByLine(file) {
   // ('\r\n') in input.txt as a single line break.
 
   let sum = 0;
+  let numbers = [
+    ["one", 1],
+    ["two", 2],
+    ["three", 3],
+    ["four", 4],
+    ["five", 5],
+    ["six", 6],
+    ["seven", 7],
+    ["eight", 8],
+    ["nine", 9],
+    ["1", 1],
+    ["2", 2],
+    ["3", 3],
+    ["4", 4],
+    ["5", 5],
+    ["6", 6],
+    ["7", 7],
+    ["8", 8],
+    ["9", 9],
+  ];
 
   for await (const line of rl) {
     // console.log(line);
-    let num = "";
-    for(char of line) {
-        if(!Number.isNaN(parseInt(char))) {
-            num += char;
-            break;
+    let firstIndex = Infinity;
+    let firstDigit = 0;
+    for ([number, integer] of numbers) {
+      let index = line.indexOf(number);
+      if (index !== -1) {
+        firstIndex = Math.min(firstIndex, index);
+        if (index === firstIndex) {
+          firstDigit = integer;
         }
+      }
     }
-    for(char of line.split('').reverse().join('')) {
-        if(!Number.isNaN(parseInt(char))) {
-            num += char;
-            break;
+    let lastIndex = -1;
+    let lastDigit = 0;
+    for ([number, integer] of numbers) {
+      let index = line.lastIndexOf(number);
+      if (index !== -1) {
+        lastIndex = Math.max(lastIndex, index);
+        if(index === lastIndex) {
+          lastDigit = integer;
         }
+      }
     }
-    // console.log(num);
+    const num = firstDigit * 10 + lastDigit;
+    // console.log(firstDigit, lastDigit, line, num);
     sum += parseInt(num);
   }
 
