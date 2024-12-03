5653c3d7d2eb0f38daffc10204d032e6cf96c7bb
Franziska Hinkelmann <franziska.hinkelmann@gmail.com>
2024-12-03 11:12:42 -0500

    Day 1, both parts

--- 2024/day1.js
+++ 2024/day1.js
@@ -1,29 +1,68 @@
 const fs = require('fs').promises;
 
-let readInput = async() => {
-    let res = await fs.readFile('./input.txt');
-    return res.toString().split('\n').map(Number);
+let readInput = async () => {
+    let res = await fs.readFile('./../input.txt');
+    res = res.toString().split('\n')
+        .map(line => line.split(/\s+/).map(Number));
+    console.log(res);
+    return res;
+
 }
 
-let findFrequency = async (inputs) => {
-    let frequencies = new Set();
-    let f = 0;
+let findDist = async (inputs) => {
+    // input array of 2 number pairs
+    let first = [];
+    let second = [];
+    for (const [f, s] of inputs) {
+        first.push(f)
+        second.push(s)
+    }
 
-    while (true) {
-        for(const input of inputs) {
-            f += input;
-            if(frequencies.has(f)) {
-                return f;
-            }
-            frequencies.add(f);
+    first.sort((a, b) => a - b)
+    second.sort((a, b) => a - b)
+
+    let sum = 0;
+    for (let i = 0; i < first.length; i++) {
+        sum += Math.abs(first[i] - second[i])
+    }
+
+    console.log(first)
+    return sum
+}
+
+const findSim = (inputs) => {
+    const first = [];
+    const second = [];
+    for (const [f, s] of inputs) {
+        first.push(f)
+        second.push(s)
+    }
+
+    const m = new Map();
+    for (const num of second) {
+        if (!m.has(num)) {
+            m.set(num, 1)
+        } else {
+            m.set(num, m.get(num) + 1)
         }
     }
-} 
+    console.log(m)
 
-let main = async() => {
+    let sum = 0
+    for(const num of first) {
+        if(m.has(num)) {
+            sum += num * m.get(num)
+        }
+    }
+    return sum
+}
+
+let main = async () => {
     let inputs = await readInput();
-    let f = await findFrequency(inputs);
-    console.log(f);
+    let sum = await findDist(inputs);
+    let simDist = await findSim(inputs);
+    console.log("sim Score: " + simDist);
+    console.log(sum);
 }
 
 main();
