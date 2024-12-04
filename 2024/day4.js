b132729877f1c22f8dc95b0dbc7ce0722ce319db
Franziska Hinkelmann <franziska.hinkelmann@gmail.com>
2024-12-04 12:31:35 -0500

    Day 4, part 2

--- 2024/day4.js
+++ 2024/day4.js
@@ -10,87 +10,52 @@ let readInput = async () => {
 }
 
 const countWords = (lines) => {
-    // XMAS
+    // MAS
     let count = 0
-    for (const line of lines) {
-        // left to right
-        let ms = line.matchAll(/XMAS/g);
-        for (const m of ms) {
-            count++
-        }
-
-        // right to left
-        ms = line.matchAll(/SAMX/g);
-        for (const m of ms) {
-            count++
-        }
-    }
 
     // top to bottom
     const n = lines.length;
     const m = lines[0].length;
-    for (let i = 0; i < n - 3; i++) {
-        let matches = [...lines[i].matchAll(/X/g)].map(m => m.index);
+    for (let i = 0; i < n - 2; i++) {
+        const matches = [...lines[i].matchAll(/M/g)].map(m => m.index);
         for (const index of matches) {
-            if (lines[i + 1][index] === 'M' && lines[i + 2][index] === 'A' && lines[i + 3][index] === 'S') {
-                count++
+            if (index >= m - 2) continue
+            if (lines[i + 1][index + 1] === 'A' && lines[i + 2][index + 2] === 'S') {
+                // M   M
+                //   A
+                // S   S
+                if (lines[i][index + 2] === 'M' && lines[i + 2][index] === 'S') {
+                    count++
+                    // M   S
+                    //   A
+                    // M   S
+                } else if (lines[i][index + 2] === 'S' && lines[i + 2][index] === 'M') {
+                    count++
+                }
             }
         }
     }
 
-    // bottom to top
-    for (let i = 0; i < n - 3; i++) {
-        let matches = [...lines[i].matchAll(/S/g)].map(m => m.index);
+    for (let i = 0; i < n - 2; i++) {
+        const matches = [...lines[i].matchAll(/S/g)].map(m => m.index);
         for (const index of matches) {
-            if (lines[i + 1][index] === 'A' && lines[i + 2][index] === 'M' && lines[i + 3][index] === 'X') {
-                count++
+            if (index >= m - 2) continue
+            if (lines[i + 1][index + 1] === 'A' && lines[i + 2][index + 2] === 'M') {
+                // S   M
+                //   A
+                // S   M
+                if (lines[i][index + 2] === 'M' && lines[i + 2][index] === 'S') {
+                    count++
+                    // S   S
+                    //   A
+                    // M   M
+                } else if (lines[i][index + 2] === 'S' && lines[i + 2][index] === 'M') {
+                    count++
+                }
             }
         }
     }
 
-    // upper left to lower right diagonal
-    for (let i = 0; i < n - 3; i++) {
-        let matches = [...lines[i].matchAll(/X/g)].map(m => m.index);
-        for (const index of matches) {
-            if (index >= m - 3) continue
-            if (lines[i + 1][index + 1] === 'M' && lines[i + 2][index + 2] === 'A' && lines[i + 3][index + 3] === 'S') {
-                count++
-            }
-        }
-    }
-
-    // lower right  to upper left diagonal
-    for (let i = 0; i < n - 3; i++) {
-        let matches = [...lines[i].matchAll(/S/g)].map(m => m.index);
-        for (const index of matches) {
-            if (index >= m - 3) continue
-            if (lines[i + 1][index + 1] === 'A' && lines[i + 2][index + 2] === 'M' && lines[i + 3][index + 3] === 'X') {
-                count++
-            }
-        }
-    }
-
-    // upper right to lower left diagonal
-    for (let i = 0; i < n - 3; i++) {
-        let matches = [...lines[i].matchAll(/X/g)].map(m => m.index);
-        for (const index of matches) {
-            if (index < 3) continue
-            if (lines[i + 1][index - 1] === 'M' && lines[i + 2][index - 2] === 'A' && lines[i + 3][index - 3] === 'S') {
-                count++
-            }
-        }
-    }
-
-    //  lower left to upper right diagonal
-    for (let i = 0; i < n - 3; i++) {
-        let matches = [...lines[i].matchAll(/S/g)].map(m => m.index);
-        for (const index of matches) {
-            if (index < 3) continue
-            if (lines[i + 1][index - 1] === 'A' && lines[i + 2][index - 2] === 'M' && lines[i + 3][index - 3] === 'X') {
-                count++
-            }
-        }
-    }
 
     return count
 
