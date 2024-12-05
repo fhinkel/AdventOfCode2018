67a813c78dac28cffe5a0921ea617f2c97a0b5e4
Franziska Hinkelmann <franziska.hinkelmann@gmail.com>
2024-12-05 11:08:57 -0500

    Split input for Day 5 in two parts

--- 2024/day5.js
+++ 2024/day5.js
@@ -1,10 +1,14 @@
+const { UV_UDP_REUSEADDR } = require('constants');
 const fs = require('fs');
 
 let readInput = () => {
-    let res = fs.readFileSync('./input.txt');
-    // let res =  fs.readFileSync('./test-input.txt');
+    // let res = fs.readFileSync('./input.txt');
+    let res =  fs.readFileSync('./test-input.txt');
     res = res.toString().split('\n').map(line => line.trim())
-    return res;
+    const index = res.indexOf("")
+    const orders = res.slice(0, index)
+    const updates = res.slice(index+1)
+    return [orders, updates];
 }
 
 const countWords = (lines) => {
