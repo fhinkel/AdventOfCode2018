6bca3a16d9f090c8243b36ed3d76c0ca7c599b19
Franziska Hinkelmann <franziska.hinkelmann@gmail.com>
2024-12-12 12:54:52 -0500

    Starting day 9

--- 2024/day9.js
+++ 2024/day9.js
@@ -1,97 +1,57 @@
 const fs = require('fs');
 
 let readInput = () => {
-    let res = fs.readFileSync('./input.txt');
-    // let res = fs.readFileSync('./test-input.txt');
-    res = res.toString().split('\n').map(line => line.trim())
+    // let res = fs.readFileSync('./input.txt');
+    let res = fs.readFileSync('./test-input.txt');
+    res = res.toString().split('').map(Number)
     return res;
 }
 
-const uniqueLocations = (arr) => {
-    const map = new Map(); // key: frequency symbol, value: array of pairs of coordinates
-    const height = arr.length
-    const width = arr[0].length
-
-    for (let i = 0; i < height; i++) {
-        for (let j = 0; j < width; j++) {
-            const symbol = arr[i][j]
-            if (symbol !== '.') {
-                if (!map.has(symbol)) {
-                    map.set(symbol, [[i, j]])
-                } else {
-                    map.get(symbol).push([i, j])
-                }
+const checksum = (arr) => {
+    console.log(arr)
+    let res = []
+    let id = 0
+    for (let i = 0; i < arr.length; i++) {
+        if(i%2 === 0)  {
+            let count = arr[i] 
+            for(let j = 0; j < count; j++) {
+                res.push(id)
+            }
+            id++
+        } else {
+            let count = arr[i] 
+            for(let j = 0; j < count; j++) {
+                res.push('.')
             }
         }
     }
+    // 00...111...2...333.44.5555.6666.777.888899
+    console.log(res.join(''))
 
-    // will hold antinode locations
-    let res = new Array(height)
-    for (let i = 0; i < height; i++) {
-        res[i] = new Array(width)
-    }
+    let empty = 0
+    let file = res.length -1
 
-    const isInBounds = (pair) => {
-        if (pair[0] < 0) return false
-        if (pair[1] < 0) return false
-        if (pair[0] >= height) return false
-        if (pair[1] >= width) return false
-        return true
-    }
-
-    for (const [, pairs] of map.entries()) {
-
-        for (let i = 0; i < pairs.length - 1; i++) {
-            for (j = i + 1; j < pairs.length; j++) {
-                const a = pairs[i]
-                const b = pairs[j]
-                let heightDiff = a[0] - b[0]
-                let widthDiff = a[1] - b[1]
-
-                let antinode = [a[0], a[1]]
-                while (isInBounds(antinode)) {
-                    res[antinode[0]][antinode[1]] = '#'
-                    antinode = [antinode[0] + heightDiff, antinode[1] + widthDiff]
-                }
-
-                antinode = [a[0] - heightDiff, a[1] - widthDiff]
-                while (isInBounds(antinode)) {
-                    res[antinode[0]][antinode[1]] = '#'
-                    antinode = [antinode[0] - heightDiff, antinode[1] - widthDiff]
-                }
-                // x .
-                // . x
-                // [0,0], [1,1
-                // diff [-1, -1]
-
-                // x..
-                // ...
-                // ..x
-
-                // . X
-                // X . 
-                // [0,1], [1,0]
-                // diff[-1, 1]
-            }
+    while(empty < file) {
+        while(res[empty] !== '.') {
+            empty++
         }
-    }
-
-    let count = 0
-    for (const line of res) {
-        for (const el of line) {
-            if (el === '#') {
-                count++
-            }
+        while(res[file] === '.') {
+            file--
         }
+        res[empty] = res[file]
+        res[file] = '.'
+        empty++
+        file--
     }
-    return count
+    // 0099811188827773336446555566..............
+    console.log(res.join(''))
 }
 
 
 let main = () => {
     let arr = readInput();
-    let locs = uniqueLocations(arr);
-    console.log(locs);
+    let sum = checksum(arr);
+    console.log(sum);
 }
 
 main();
